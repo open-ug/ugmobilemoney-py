98be0b7d9347bc9110bc165a8ab6506da2f24b1e
jim-junior <jimjunior854@gmail.com>
2024-03-15 21:33:14 +0300

    iid

--- mobile_money/yo/withdraw.py
+++ mobile_money/yo/withdraw.py
@@ -3,7 +3,7 @@ import httpx
 # import xmltodict
 
 SANDBOX_URL = "https://sandbox.yo.co.ug/services/yopaymentsdev/task.php"
-PRODUCTION_URL = "https://sandbox.yo.co.ug/services/yopaymentsdev/task.php"
+PRODUCTION_URL = "https://paymentsapi1.yo.co.ug/ybs/task.php"
 
 
 def withdraw(
